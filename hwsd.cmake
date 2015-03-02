18441000d4ad3e2bba8cf995206e5407ef7e1415
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-03-02 09:41:03 +0100

    hwsd : add QtCore for missing QString symbols

--- hwsd.cmake
+++ hwsd.cmake
@@ -4,9 +4,9 @@ set(HWSD_REPO_URL https://github.com/Eyescale/hwsd.git)
 set(HWSD_REPO_TAG master)
 set(HWSD_DEPENDS OpenGL Qt4 X11 REQUIRED Lunchbox Boost)
 set(HWSD_BOOST_COMPONENTS "program_options regex system")
-set(HWSD_QT4_COMPONENTS "QtNetwork")
+set(HWSD_QT4_COMPONENTS "QtCore QtNetwork")
 set(HWSD_DEB_DEPENDS libx11-dev libgl1-mesa-dev libqt4-dev
-  libboost-program-options-dev libboost-regex-dev 
+  libboost-program-options-dev libboost-regex-dev
   libboost-system-dev)
 set(HWSD_PORT_DEPENDS boost qt4-mac)
 set(HWSD_FORCE_BUILD ${CI_BUILD})
