46025ab3d48a8087fbc8db4b06be2482589e91f2
Daniel Nachbaur <danielnachbaur@gmail.com>
2015-04-28 16:22:34 +0200

    Port to Qt5

--- Equalizer.cmake
+++ Equalizer.cmake
@@ -5,15 +5,14 @@ set(EQUALIZER_REPO_URL https://github.com/Eyescale/Equalizer.git)
 set(EQUALIZER_DEPENDS
   REQUIRED vmmlib Lunchbox Collage OpenGL Boost Pression
   OPTIONAL X11 hwsd GLStats hwloc OpenSceneGraph OpenCV VRPN
-           Deflect MAGELLAN GLEW_MX Qt4)
+           Deflect MAGELLAN GLEW_MX Qt5Core Qt5OpenGL Qt5Widgets)
 set(EQUALIZER_BOOST_COMPONENTS "program_options filesystem system thread")
 set(EQUALIZER_OPENSCENEGRAPH_COMPONENTS "osgDB osgUtil")
-set(EQUALIZER_QT4_COMPONENTS "QtCore QtGui QtOpenGL")
 
 set(EQUALIZER_DEB_DEPENDS bison flex libboost-program-options-dev
   libboost-filesystem-dev libboost-system-dev libboost-thread-dev
   libx11-dev libgl1-mesa-dev libglewmx1.6-dev libspnav-dev
-  libopenscenegraph-dev libopencv-dev libqt4-dev)
+  libopenscenegraph-dev libopencv-dev qtbase5-dev)
 set(EQUALIZER_PORT_DEPENDS boost opencv)
 set(EQUALIZER_ROOT_VAR EQ_ROOT)
 set(EQUALIZER_SUBPROJECT ON)
